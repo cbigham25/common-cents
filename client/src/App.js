bde435caacdfbc443bb8903be5f30a23686e947c
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-08-09 20:54:16 -0500

    added header and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import React from 'react';
-import { LoginPage } from './components/Login/LoginPage';
-import { SignUpPage } from './components/Signup/SignupPage';
+import LoginPage from './components/Login/LoginPage';
+import SignUpPage from './components/Signup/SignupPage';
+import BudgetForm from './components/Budget/Index'
+import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
-
+//app is defined but never used??
+// It was because there were two "App()" functions 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-
-
-  //app is defined but never used??
-  function App() {
-    return (
-      <div className="App">
-        <LoginPage />
-        <SignUpPage />
+  return (<ApolloProvider client={client}>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={<BudgetForm />}
+            />
+            <Route
+              path="/login"
+              element={<LoginPage />}
+            />
+            <Route
+              path="/signup"
+              element={<SignUpPage />}
+            />
+          </Routes>
+        </div>
       </div>
-    );
-  }
+    </Router>
+  </ApolloProvider>
+  );
 }
+
 
 
 export default App;
