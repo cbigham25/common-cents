7f0bd4a9119c920636af39cef04925c9f6e56a63
ToriKnix <toriknixmusic@gmail.com>
2023-08-10 19:42:06 -0600

    revamped the budget.js page to have the list of items we agreed on in the mockup

--- client/src/components/Budget/Budget.js
+++ client/src/components/Budget/Budget.js
@@ -1,17 +1,50 @@
-import React from "react";
-//import Auth from "../utils/auth";
-import { useState } from 'react';
+import React, { useState } from "react";
 
 const BudgetForm = ({ onSubmit }) => {
   const [budgetName, setBudgetName] = useState('');
   const [amount, setAmount] = useState('');
+  const [selectedNeed, setSelectedNeed] = useState('Groceries');
+  const [needsList] = useState([
+    'Groceries',
+    'Gas',
+    'Debt',
+    'Rent',
+    'Bills',
+    'Other'
+  ]);
+  const [needAmounts, setNeedAmounts] = useState({
+    Groceries: '',
+    Gas: '',
+    Debt: '',
+    Rent: '',
+    Bills: '',
+    Other: ''
+  });
+
+  const handleNeedAmountChange = (need, value) => {
+    setNeedAmounts(prevAmounts => ({ ...prevAmounts, [need]: value }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (budgetName && amount) {
-      onSubmit({ name: budgetName, amount });
+    if (budgetName && amount && selectedNeed) {
+      onSubmit({
+        name: budgetName,
+        amount,
+        need: selectedNeed,
+        needAmounts: { ...needAmounts }
+      });
       setBudgetName('');
       setAmount('');
+      setSelectedNeed('Groceries');
+      setNeedAmounts({
+        Groceries: '',
+        Gas: '',
+        Debt: '',
+        Rent: '',
+        Bills: '',
+        Other: ''
+      });
     }
   };
 
@@ -26,6 +59,24 @@ const BudgetForm = ({ onSubmit }) => {
         <div>
           <label>Amount:</label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </div>
+        <div>
+          <label>Need:</label>
+          <select value={selectedNeed} onChange={(e) => setSelectedNeed(e.target.value)}>
+            {needsList.map((need) => (
+              <option key={need} value={need}>
+                {need}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Amount for {selectedNeed}:</label>
+          <input
+            type="number"
+            value={needAmounts[selectedNeed]}
+            onChange={(e) => handleNeedAmountChange(selectedNeed, e.target.value)}
+          />
         </div>
         <button type="submit">Create Budget</button>
       </form>
