719ad6e1accd00beab790faf4aaf7da5ad3046d2
cbigham25 <81174295+cbigham25@users.noreply.github.com>
2023-08-16 19:24:40 -0500

    added conditional login

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EditBudget from './components/Edit-Budget/editBudget';
+import Auth from './components/utils/auth'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
--- client/src/components/Budget/Budget.js
+++ client/src/components/Budget/Budget.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries.js';
@@ -29,39 +29,52 @@ const BudgetForm = () => {
       return acc;
     }, {});
   }
-  
-  const expenses = data.user.expenses;
-  const aggregatedExpenses = aggregateExpensesByCategory(expenses);
-  
+
+  const expenses = data?.user?.expenses;
+
+  let aggregatedExpenses = 0;
+
+  if (expenses) {
+    aggregatedExpenses = aggregateExpensesByCategory(expenses);
+  }
+
   console.log(aggregatedExpenses);
-  
+
 
   return (
-    <section className='budgetContainer'>
-      <h1>Hello, {Auth.getProfile().data.username}</h1>
-      <p>Your total expenses: {totalExpenses}</p>
-      <section className="needsContainer">
-        <h2>Needs</h2>
-        <p>Rent: {aggregatedExpenses.Rent || 0}</p>
-        <p>Utilities:{aggregatedExpenses.Utilities || 0}</p>
-        <p>Taxes: {aggregatedExpenses.Taxes || 0}</p>
-        <p>Insurance: {aggregatedExpenses.Insurance || 0}</p>
-        <p>Bills: {aggregatedExpenses.Bills || 0}</p>
-        <p>Health: {aggregatedExpenses.Health || 0}</p>
-        <p>Groceries: {aggregatedExpenses.Groceries || 0}</p>
-        <p>Debt: {aggregatedExpenses.Debt || 0}</p>
-        <p>Other: {aggregatedExpenses.OtherNeeds || 0}</p>
-      </section>
-      <section className="wantsContainer">
-        <h2>Wants</h2>
-        <p>Dining: {aggregatedExpenses.Dining || 0}</p>
-        <p>Fun: {aggregatedExpenses.Fun || 0}</p>
-        <p>Products: {aggregatedExpenses.Products || 0}</p>
-        <p>Clothing: {aggregatedExpenses.Clothing || 0}</p>
-        <p>Vacation: {aggregatedExpenses.Vacation || 0}</p>
-        <p>Other: {aggregatedExpenses.OtherWants || 0}</p>
-      </section>
-    </section>
+    <div>
+      {Auth.loggedIn() ? (
+        <>
+          <section className='budgetContainer'>
+            <h1>Hello, {Auth.getProfile().data.username}</h1>
+            <p>Your total expenses: {totalExpenses}</p>
+            <section className="needsContainer">
+              <h2>Needs</h2>
+              <p>Rent: {aggregatedExpenses.Rent || 0}</p>
+              <p>Utilities:{aggregatedExpenses.Utilities || 0}</p>
+              <p>Taxes: {aggregatedExpenses.Taxes || 0}</p>
+              <p>Insurance: {aggregatedExpenses.Insurance || 0}</p>
+              <p>Bills: {aggregatedExpenses.Bills || 0}</p>
+              <p>Health: {aggregatedExpenses.Health || 0}</p>
+              <p>Groceries: {aggregatedExpenses.Groceries || 0}</p>
+              <p>Debt: {aggregatedExpenses.Debt || 0}</p>
+              <p>Other: {aggregatedExpenses.OtherNeeds || 0}</p>
+            </section>
+            <section className="wantsContainer">
+              <h2>Wants</h2>
+              <p>Dining: {aggregatedExpenses.Dining || 0}</p>
+              <p>Fun: {aggregatedExpenses.Fun || 0}</p>
+              <p>Products: {aggregatedExpenses.Products || 0}</p>
+              <p>Clothing: {aggregatedExpenses.Clothing || 0}</p>
+              <p>Vacation: {aggregatedExpenses.Vacation || 0}</p>
+              <p>Other: {aggregatedExpenses.OtherWants || 0}</p>
+            </section>
+          </section>
+        </>) : (
+        <p>Please login to or create your account from the links above! </p>
+      )
+      }
+    </div>
   );
 };
 
