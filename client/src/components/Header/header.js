692c10620a510ef467b74a705af175d220668f55
benos <benantonn@gmail.com>
2023-08-31 11:09:04 -0500

    add date selection in header auth dependent

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -13,27 +13,81 @@ const Header = () => {
       <Link to="/">
         <h1 >Common Cents</h1>
       </Link>
-      <div>
-        {Auth.loggedIn() ? (
-          <>
+      {Auth.loggedIn() ? (
+        <>
+          <section className='dateInput'>
+            <select name="months" id="months">
+              <option value="01">
+                January
+              </option>
+              <option value="02">
+                February
+              </option>
+              <option value="03">
+                March
+              </option>
+              <option value="04">
+                April
+              </option>
+              <option value="05">
+                May
+              </option>
+              <option value="06">
+                June
+              </option>
+              <option value="07">
+                July
+              </option>
+              <option value="08">
+                August
+              </option>
+              <option value="09">
+                September
+              </option>
+              <option value="10">
+                October
+              </option>
+              <option value="11">
+                November
+              </option>
+              <option value="12">
+                December
+              </option>
+            </select>
+            <input
+              type="number"
+              id="year"
+              name="year"
+              min="1900"
+              max="2099"
+              step="1"
+              minLength="4"
+              maxLength="4"
+              required
+            />
+            <button>Go</button>
+          </section>
+          <div>
             <Link to="/me">
               {Auth.getProfile().data.username}'s profile
             </Link>
             <button onClick={logout}>
               Logout
             </button>
-          </>
-        ) : (
-          <>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
             <Link to="/login">
               Login
             </Link>
             <Link to="/signup">
               Signup
             </Link>
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </header>
   );
 };
