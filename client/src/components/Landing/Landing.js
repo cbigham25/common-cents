086326ecfcab345f30413027bb5092512433cda7
benos <benantonn@gmail.com>
2023-10-04 15:30:00 -0400

    add copy to landing page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Landing() {
-  return (
-    <h1>The easiest way to budget</h1>
-  )
+    return (
+        <div>
+            <h2>The easiest way to budget</h2>
+            <h1>Common ₵ents</h1>
+            <p>The 50-30-20 principle is a simple approach to budgeting where 50 percent of your income goes to needs, 30 percent goes to wants, and 20 percent goes to debt and taxes. </p>
+        </div>
+    )
 }
 
 export default Landing
