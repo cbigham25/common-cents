858ecd148b2431ded196b8d760089ffcfe89ed63
benos <benantonn@gmail.com>
2023-10-07 15:30:00 -0400

    update landing copy

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -5,8 +5,19 @@ function Landing() {
     return (
         <div>
             <section className='landingSection'>
-                <h1>Common ₵ents</h1>
-                <h2>The easiest way to budget</h2>
+                <div>
+                    <h1>
+                        <span>The</span>
+                        <span> EASIEST </span>
+                        <span>way to budget your life!</span>
+                    </h1>
+                    <p>
+                        Tired of overspending but don't know where to start? Look no further! Fix your financial health today with the most simple approch to setting a budget. No prior knowledge needed!
+                    </p>
+                </div>
+                <div>
+                    
+                </div>
             </section>
             <section>
                 <p>The 50-30-20 principle is a simple approach to budgeting where 50 percent of your income goes to needs, 30 percent goes to wants, and 20 percent goes to debt and taxes. </p>
