95645034331b3b6bd5e1092b18abdc7573b50367
benos <benantonn@gmail.com>
2023-10-09 15:30:00 -0400

    add landing image and tile, styling

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './landing.css';
+import budget from '../../assets/budget.jpg'
 
 function Landing() {
     return (
@@ -16,15 +17,31 @@ function Landing() {
                     <p>
                         Tired of overspending but don't know where to start? Look no further! Fix your financial health today with the most simple approch to setting a budget. No prior knowledge needed!
                     </p>
-                    <button>Try It Now</button>
-                    <button>View An Example</button>
+                    <div className='headerButtons'>
+                        <button>Try It Now</button>
+                        <button>View An Example</button>
+                    </div>
                 </div>
-                <div className='landingSubSection'>
-
+                <div className='landingSubSection landingImageHeader'>
+                    <img src={budget} alt="budget" />
                 </div>
             </section>
             <section>
-                <p>The 50-30-20 principle is a simple approach to budgeting where 50 percent of your income goes to needs, 30 percent goes to wants, and 20 percent goes to debt and taxes. </p>
+                <div className='landingTile'>
+                    <div className='landingTileNumber'>
+                        <div className='landingTileNumberTile'>
+                            <h1>50</h1>
+                        </div>
+                        <div className='landingTileNumberTile'>
+                            <h1>30</h1>
+                        </div>
+                        <div className='landingTileNumberTile'>
+                            <h1>20</h1>
+                        </div>
+                    </div>
+                    <h1> </h1>
+                    <p>The 50-30-20 principle is a simple approach to budgeting where 50 percent of your income goes to needs, 30 percent goes to wants, and 20 percent goes to debt and taxes. </p>
+                </div>
             </section>
         </div>
     )
