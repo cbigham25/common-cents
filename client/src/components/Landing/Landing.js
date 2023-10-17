a9a5907e0bf7746e0694186fe97ccb1479f3ef4c
benos <benantonn@gmail.com>
2023-10-17 15:30:00 -0400

    update ladning copy and styling

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -28,6 +28,7 @@ function Landing() {
             </section>
             <section>
                 <div className='landingTile'>
+                    <h1>How does it work?</h1>
                     <div className='landingTileNumber'>
                         <div className='landingTileNumberTile'>
                             <h1>50</h1>
@@ -39,8 +40,18 @@ function Landing() {
                             <h1>20</h1>
                         </div>
                     </div>
-                    <h1> </h1>
-                    <p>The 50-30-20 principle is a simple approach to budgeting where 50 percent of your income goes to needs, 30 percent goes to wants, and 20 percent goes to debt and taxes. </p>
+                    <p>The 50-30-20 principle is a simple approach to budgeting where 50 percent of your income goes to needs, 30 percent goes to wants, and 20 percent goes to debt and taxes. Common Cents implements this methodology for you in 2 simple steps.</p>
+                    <div className='landingTileSteps'>
+                        <div className='landingTileStepCard'>
+                            <h1>1</h1>
+                            <h2>Enter your income</h2>
+                        </div>
+                        <div className='landingTileStepCard'>
+                            <h1>2</h1>
+                            <h2>Enter your expenses</h2>
+                        </div>
+                    </div>
+                    <p>and Common Cents does the rest! In your budget dashboard you will be able to see a comprehensive breakdown of your monthly spending and how much you have left to spend in each category.</p>
                 </div>
             </section>
         </div>
