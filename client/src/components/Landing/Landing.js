c82c7e46e0a0e4c727a17715309c41cba8795eed
benos <benantonn@gmail.com>
2023-10-19 15:30:00 -0400

    add subtext to header image

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -18,12 +18,32 @@ function Landing() {
                         Tired of overspending but don't know where to start? Look no further! Fix your financial health today with the most simple approch to setting a budget. No prior knowledge needed!
                     </p>
                     <div className='headerButtons'>
-                        <button>Try It Now</button>
-                        <button>View An Example</button>
+                        <button><strong>Try It Now</strong></button>
+                        <button><strong>View An Example</strong></button>
                     </div>
                 </div>
                 <div className='landingSubSection landingImageHeader'>
                     <img src={budget} alt="budget" />
+                    <div className='landingImageHeaderSubtextContainer'>
+                        <div className='landingImageHeaderSubtext'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='landingImageHeaderCheck'>
+                                <path d="m12 0a12 12 0 1 0 12 12 12.014 12.014 0 0 0 -12-12zm6.927 8.2-6.845 9.289a1.011 1.011 0 0 1 -1.43.188l-4.888-3.908a1 1 0 1 1 1.25-1.562l4.076 3.261 6.227-8.451a1 1 0 1 1 1.61 1.183z"></path>
+                            </svg>
+                            <p>100% Free Forever</p>
+                        </div>
+                        <div className='landingImageHeaderSubtext'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='landingImageHeaderCheck'>
+                                <path d="m12 0a12 12 0 1 0 12 12 12.014 12.014 0 0 0 -12-12zm6.927 8.2-6.845 9.289a1.011 1.011 0 0 1 -1.43.188l-4.888-3.908a1 1 0 1 1 1.25-1.562l4.076 3.261 6.227-8.451a1 1 0 1 1 1.61 1.183z"></path>
+                            </svg>
+                            <p>Easy to Use</p>
+                        </div>
+                        <div className='landingImageHeaderSubtext'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='landingImageHeaderCheck'>
+                                <path d="m12 0a12 12 0 1 0 12 12 12.014 12.014 0 0 0 -12-12zm6.927 8.2-6.845 9.289a1.011 1.011 0 0 1 -1.43.188l-4.888-3.908a1 1 0 1 1 1.25-1.562l4.076 3.261 6.227-8.451a1 1 0 1 1 1.61 1.183z"></path>
+                            </svg>
+                            <p>Secure</p>
+                        </div>
+                    </div>
                 </div>
             </section>
             <section>
